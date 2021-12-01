c3634d150799f63e22d7efaeacd8497453846822
Aurelie LECONTE <aleconte@e1r5p13.42.fr>
2021-12-01 15:48:15 +0100

    cpp06 ex02

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <ctime>
+
+Base * generate(void)
+{
+	switch (std::rand() % 3) {
+		case 0:
+			std::cout << "class a" << std::endl;
+			return (new A);
+		case 1:
+			std::cout << "class b" << std::endl;
+			return (new B);
+		case 2:
+			std::cout << "class c" << std::endl;
+			return (new C);	
+	}
+	return (nullptr);
+}
+
+void identify(Base * p)
+{
+	if(dynamic_cast<A*>(p))
+		std::cout << "Is class A" << std::endl;
+	else if(dynamic_cast<B*>(p))
+		std::cout << "Is class B" << std::endl;
+	else if(dynamic_cast<C*>(p))
+		std::cout << "Is class C" << std::endl;
+}
+
+void identify(Base & p)
+{
+	try {
+		A a = dynamic_cast<A&>(p);
+		std::cout << "Is class A" << std::endl;
+		return ;
+	}
+	catch(const std::exception &e) {}
+	try {
+		B b = dynamic_cast<B&>(p);
+		std::cout << "Is class B" << std::endl;
+		return ;
+	}
+	catch(const std::exception &e) {}
+	try {
+		C c = dynamic_cast<C&>(p);
+		std::cout << "Is class C" << std::endl;
+		return ;
+	}
+	catch(const std::exception &e) {}
+}
+
+int main()
+{
+	std::srand(std::time(nullptr));
+	Base *base = generate();
+	if (!base)
+		return 1;
+	identify(base);
+	identify(*base);
+	return(0);
+}
